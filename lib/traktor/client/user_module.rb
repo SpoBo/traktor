1e5405c6706bc47a44e6fe2ad60053544bf442f6
Vincent De Snerck <thespobo@gmail.com>
2011-01-27 21:34:38 +0100

    UserModule::ensure_user method added
    Added a method that makes it possible for methods to run code in a block that tries to get the global username first if needed and run the provided code. If no user could be found it throws an exception. Neat way to get similar logic for all functions. Loving Ruby already :p

--- lib/traktor/client/user_module.rb
+++ lib/traktor/client/user_module.rb
@@ -5,14 +5,20 @@ module Traktor
     module UserModule
 
       def watched_movies(user=nil)
-        user ||= @user
-
-        raise NoUserException.new() if user.nil?
-
-        JSON.parse(RestClient.get("#{@endpoint}/user/watched/movies.json/#{@api_key}/#{user}", :accept => :json)).
-          map { |o| Movie.new(o['movie']) }
+        ensure_user(user) do |user|
+          JSON.parse(RestClient.get("#{@endpoint}/user/watched/movies.json/#{@api_key}/#{user}", :accept => :json)).
+            map { |o| Movie.new(o['movie']) }
+        end
       end
 
+      protected
+
+        def ensure_user(user)
+          user ||= @user
+          raise NoUserException.new() if user.nil?
+
+          yield user # do the regular logic.
+        end
     end
   end
 end
